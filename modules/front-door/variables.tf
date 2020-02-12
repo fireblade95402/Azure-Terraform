328a5964f9f049b319b987663745d43403f96d29
Mark <34512169+fireblade95402@users.noreply.github.com>
2020-02-12 14:40:22 +0000

    Update variables.tf

--- modules/front-door/variables.tf
+++ modules/front-door/variables.tf
@@ -1,35 +1,19 @@
-variable "az_afd_name" {
-  description = "(Required) Name of the Azure Front Door to be created"  
+variable "front-door-rg" {
+  description = "(Required) Resource Group of the Front Door to be created"  
 }
 
 variable "location" {
-  description = "(Required) Location of the Azure  Front Door to be created"  
+  description = "(Required) Location of the Front Door to be created"  
+}
+
+variable "front-door-object" {
+  description = "(Required) Front Door Object configuration"  
+}
+
+variable "front-door-waf-object" {
+    description = "(Required) Front Door WAF Object configuration"
 }
 
 variable "tags" {
-  description = "(Required) Tags of the Azure  Front Door to be created"  
-}
-
-variable "az_afd_rg" {
-  description = "(Required) Resource Group of the Azure  Front Door to be created"  
-}
-
-variable "front-door-settings" {
-  description = "(Required) AFD Settings of the Azure  Front Door to be created"  
-}
-
-variable "front-door-waf-settings" {
-    description = "(Required) Azure Front Door settings"
-}
-
-variable "ARM_LOCATION" {
-    description = "The geo location to which the resources are being deployed"
-}
-
-variable "ARM_ENVIRONMENT" {
-    description = "The deployment environment acronym to which the resources are being deployed"
-}
-
-variable "ARM_SUBSCRIPTION_ID" {
-    description = "Azure Subscription for deployment"
+  description = "(Required) Tags of the Front Door to be created"  
 }
