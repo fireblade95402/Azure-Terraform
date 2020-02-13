b374270d67b4ac2811494762693d918aac6cb11e
Mark Graham <magraham@microsoft.com>
2020-02-13 20:43:42 +0000

    clean-up

--- modules/front-door/examples/sinple-fornt-door-plus-WAF/.terraform/modules/resource_groups/aztfmod-terraform-azurerm-caf-resource-group-c4340ab/examples/rg_simple/rg.tf
+++
@@ -1,8 +0,0 @@
-
-module "rg_test" {
-  source = "../../"
-  
-    prefix = local.prefix
-    resource_groups = local.resource_groups
-    tags = local.tags
-}
--- modules/front-door/examples/sinple-fornt-door-plus-WAF/.terraform/modules/rg_caftest/aztfmod-terraform-azurerm-caf-resource-group-e970ba9/module.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_resource_group" "rg" {
-   for_each = var.resource_groups
-
-         name     = "${var.prefix}${each.value.name}"
-         location = each.value.location
-         tags     = lookup(each.value, "tags", null) == null ? local.tags : merge(local.tags,each.value.tags)
-
-}
